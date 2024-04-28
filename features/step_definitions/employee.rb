c92348cbca1d4a207251cf588410badcdcd5d2fe
ViniMendes-exe <vinicius_00_00@outlook.com>
2024-04-28 20:49:55 -0300

    Criação do Post

--- features/step_definitions/employee.rb
+++ features/step_definitions/employee.rb
@@ -17,16 +17,22 @@ Dado('que o usuario cadastre um novo funcionario') do
 end
   
 Quando('ele enviar as informacoes de funcionario') do
-    @create_employee = HTTParty.post(@url, body: {
-        'employee_name': "Robin O'Reilly",
-        'employee_salary': 82295,
-        'employee_age': 32,
-        'id': ""
+    @create_employee = HTTParty.post(@url, :headers => {'Content-Type': 'application/json'}, body: {
+        "employee_name": "Vinicin do Gás",
+        "employee_salary": 82295,
+        'employee_age': 23,
+        "id": ""
     }.to_json)
 
         puts @create_employee
 end
   
   Entao('esse funcionario sera cadastrado') do
-
+    puts @create_employee.code
+    puts @create_employee.msg
+    expect(@create_employee["employee_name"]).to eql 'Vinicin do Gás'
+    expect(@create_employee["employee_salary"]).to eql (82295)
+    expect(@create_employee["employee_age"]).to eql (23)
+    expect(@create_employee.code).to eql 201
+    expect(@create_employee.message).to eql 'Created'
   end
